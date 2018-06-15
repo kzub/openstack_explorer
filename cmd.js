c4f985881040ec223d8576378a034c950752e272
Zubkov Konstantin <konstantin.zubkov@onetwotrip.com>
2018-06-15 20:26:07 +0300

    added cloud-la mode

--- cmd.js
+++ cmd.js
@@ -1,181 +1,112 @@
-const fs = require('fs');
-const promisify = require('util').promisify;
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
-const request = promisify(require('request'));
-const extend = require('util')._extend;
+const utils = require('./utils');
+const openstack = require('./openstack');
+const hypervisor = require('./hypervisor');
 
-const settingsFileName = 'settings.json';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-async function main(){
-  let cmd = process.argv[2];
+process.on('unhandledRejection', (reason) => {
+  console.log('unhandledRejection:', reason);
+  process.exit(-1);
+});
+
+async function main() {
+  const cmd = process.argv[2];
   if (cmd === 'auth') {
-    let settings = await getSettings();
-    await loadAuth(settings);
+    const settings = await utils.getSettings();
+    await openstack.loadAuth(settings);
+    return;
   }
-  else if (cmd === 'list') {
-    let l = await openStackRequest('/servers/detail');
-    console.log(JSON.stringify(l.servers[0]))
-    let map = mapArrayByValue(l.servers, 'hostId');
-  }
-  else if (cmd === 'flavors') {
-    let l = await openStackRequest('/flavors/detail');
-    console.log(JSON.stringify(l))
-    // mapArrayByValue(l.flavors, 'name');
-  }
-  else if (cmd === 'cloud') {
-    let s = await openStackRequest('/servers/detail');
-    let f = await openStackRequest('/flavors/detail');
-    let srv = mapArrayByValue(s.servers, 'OS-EXT-SRV-ATTR:hypervisor_hostname' /*'hostId'*/);
-    let flv = mapArrayByValue(f.flavors, 'id');
-    printCloud(srv, flv);
-  }
-  else if (cmd === 'cloud-json') {
-    let s = await openStackRequest('/servers/detail');
-    let srvs = mapArrayByValue(s.servers, 'name');
-    let res = mapCloudResult(srvs, 'OS-EXT-SRV-ATTR:hypervisor_hostname');
-    console.log(JSON.stringify(res));
-  }
-  else {
-    console.log('node cmd <...>\n')
-  }
-}
-main();
 
-// formating output
-// ------------------------------------------------------
-function printCloud(srv, flv) {
-  for (let s in srv) {
-    let mem = 0;
-    let cpu = 0;
-    let disk = 0;
-    console.log(s, srv[s].length);
-    for (let elm of srv[s]) {
-      let f = flv[elm.flavor.id][0];
-      console.log(elm.name, f.vcpus, f.ram, f.disk, f.name);
-      mem += f.ram;
-      cpu += f.vcpus;
-      disk += f.disk;
+  if (cmd === 'list') {
+    const l = await openstack.openStackRequest('/servers/detail');
+    console.log(JSON.stringify(l.servers[0]));
+    const map = utils.mapArrayByValue(l.servers, 'hostId');
+    console.log(map);
+    return;
+  }
+
+  if (cmd === 'flavors') {
+    const l = await openstack.openStackRequest('/flavors/detail');
+    console.log(JSON.stringify(l));
+    // utils.mapArrayByValue(l.flavors, 'name');
+    return;
+  }
+
+  if (cmd === 'cloud') {
+    const s = await openstack.openStackRequest('/servers/detail');
+    const f = await openstack.openStackRequest('/flavors/detail');
+    const srv = utils.mapArrayByValue(s.servers, 'OS-EXT-SRV-ATTR:hypervisor_hostname' /* 'hostId' */);
+    const flv = utils.mapArrayByValue(f.flavors, 'id');
+    utils.printCloud(srv, flv);
+    return;
+  }
+
+  if (cmd === 'hypervisors-la' || cmd === 'hypervisors-la2') {
+    const list = await openstack.openStackRequest('/os-hypervisors/detail');
+    const hypers = [];
+    for (const h of list.hypervisors) {
+      const uptime = await openstack.openStackRequest(`/os-hypervisors/${h.id}/uptime`, 1000);
+      hypers.push({
+        id: h.id,
+        ...uptime,
+      });
     }
-    console.log(`Total RAM:${mem}, CPU:${cpu}, DISK:${disk}`);
-    console.log();
+    if (cmd === 'hypervisors-la2') {
+      const info = hypervisor.buildHypervisorInfo(hypers);
+      info.sort((a, b) => b.avg15 - a.avg15);
+      console.log(info.map(e => [e.name, e.avg15].join('\t')).join('\n'));
+    } else {
+      const metrics = hypervisor.buildScollectorMetrics(hypers);
+      console.log(metrics);
+    }
+    return;
   }
+
+  if (cmd === 'cloud-json') {
+    const s = await openstack.openStackRequest('/servers/detail');
+    const srvs = utils.mapArrayByValue(s.servers, 'name');
+    const res = utils.mapCloudResult(srvs, 'OS-EXT-SRV-ATTR:hypervisor_hostname');
+    console.log(JSON.stringify(res));
+    return;
+  }
+
+  if (cmd === 'cloud-la') {
+    const s = await openstack.openStackRequest('/servers/detail');
+    const activeServers = s.servers.filter(a => a.status === 'ACTIVE');
+    const srvs = utils.mapArrayByValue(activeServers, 'name');
+
+    const inActive = s.servers.filter(a => a.status !== 'ACTIVE');
+    if (inActive.length) {
+      console.log(inActive.map(a => `INACTIVE: ${a.name}`).join('\n'));
+    }
+
+    const srvsHypers = utils.mapCloudResult(srvs, ['OS-EXT-SRV-ATTR:hypervisor_hostname', 'id']);
+    const hypers = hypervisor.buildHostsByHyper(srvsHypers, ['compute12.nova-msk-97.servers.com']);
+    await hypervisor.fillHypersWithVMs(hypers);
+    const sorterHypers = hypervisor.sortHypersByLA(hypers);
+
+    console.log(sorterHypers.map(e => [e.name, e.sumLA.toFixed(1), e.vms.length].join(' ')).join('\n'));
+    console.log('--------------------------');
+    console.log('avg:', (sorterHypers.reduce((a, b) => a + b.sumLA, 0) / sorterHypers.length).toFixed(1));
+
+    const spreadLA = 2;
+    const migratePlan = hypervisor.buildMigrations(sorterHypers, spreadLA);
+
+    console.log('--------------------------');
+    migratePlan.forEach((m) => {
+      console.log(`openstack server migrate ${m[2]} --live ${m[4].split('.')[0]} --block-migration # ${m[0]} ${m[1]}`);
+    });
+
+    console.log('--------------------------');
+    console.log(sorterHypers.map(e => [e.name, e.sumLA.toFixed(1), e.vms.length].join(' ')).join('\n'));
+    return;
+  }
+
+  console.log('node cmd <...>\n');
 }
 
-function printMapArray(map, field) {
-  for (let m in map) {
-    console.log(m, map[m].length);
-    console.log(map[m].map(s => s[field]).join('\n'), '\n');
-  }
-}
-
-function mapCloudResult(map, field) {
-  let res = {};
-  for (let m in map) {
-    res[m] = map[m][0][field]
-    // console.log(m, map[m][0]['OS-EXT-SRV-ATTR:hypervisor_hostname']);
-    // console.log(map[m].map(s => s.name).join('\n'), '\n');
-  }
-  return res;
-}
-
-function mapArrayByValue(list, key){
-  let map = {};
-  for (let e of list) {
-    map[e[key]] = map[e[key]] || [];
-    map[e[key]].push(e);
-  }
-  return map;
-}
-
-// openstack api connection
-// -----------------------------------------------------
-async function getSettings() {
-  let file = await readFile(settingsFileName);
-  let settings = JSON.parse(file);
-  if (!(settings && settings.auth && settings.auth.data && settings.auth.data.token)) {
-    settings = await loadAuth(settings);
-  }
-  return settings;
-}
-
-async function openStackRequest(path) {
-  try {
-    let settings = await getSettings();
-    let url = await getComputeURL(settings);
-    let r = await requestWithAuth(`${url}${path}`);
-    return JSON.parse(r.body);
-  } catch(e) {
-    console.log('openStackRequest err:', e);
-  }
-}
-
-async function getComputeURL(settings) {
-  return settings.auth.data.token.catalog
-    .filter(c =>  c.type === "compute")[0]
-    .endpoints.filter(e => e.interface === 'public')[0]
-    .url;
-}
-
-async function loadAuth(settings) {
-  try {
-    let opts = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "auth": {
-          "identity": {
-            "methods": ["password"],
-            "password": {
-              "user": {
-                "name": `${settings.credentials.username}`,
-                "domain": { "id": "default" },
-                "password": `${settings.credentials.password}`
-              }
-            }
-          }
-        }
-      })
-    };
-
-    let r = await request(`${settings.credentials.url}`, opts);
-    settings.auth = {
-      token: r.headers['x-subject-token'],
-      data: JSON.parse(r.body)
-    };
-    await writeFile(settingsFileName, JSON.stringify(settings, null, 2));
-    // console.log('well done');
-  } catch(e) {
-    console.log('err', e);
-  }
-  return settings;
-}
-
-async function requestWithAuth(url, opts) {
-  let settings = await getSettings();
-  if (!opts) {
-    opts = {};
-  }
-  if (!opts.headers) {
-    opts.headers = {};
-  }
-  if (!opts.headers['Content-Type']) {
-    opts.headers['Content-Type'] = 'application/json';
-  }
-
-  opts.headers = extend({
-    'X-Auth-Token': settings.auth.token,
-  }, opts.headers);
-
-  let r = await request(url, opts);
-  // console.log('requestWithAuth1', r.statusCode, settings.auth.token);
-  if (r.statusCode === 401) {
-    settings = await loadAuth(settings);
-    opts.headers['X-Auth-Token'] = settings.auth.token
-    r = await request(url, opts);
-    // console.log('requestWithAuth2', r.statusCode, settings.auth.token);
-  }
-  return r;
-}
+main();
+// console.log('Press any key to continue.');
+// process.stdin.once('data', () => {
+//   main();
+// });
